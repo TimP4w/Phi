ecf9dcf82c8cf4987d0fb15ed83c308de88ba84e
Timucin Besken <timucin@besken.ch>
2025-03-19 23:42:20 +0100

    fix: use wss when it's a secure connection

--- web/src/core/shared/env.ts
+++ web/src/core/shared/env.ts
@@ -1,6 +1,6 @@
 export const env = {
   API_URL: import.meta.env.VITE_URL || `${window.location.origin}`,
-  WS_URL: import.meta.env.VITE_WS || `ws://${window.location.host}/ws`,
+  WS_URL: import.meta.env.VITE_WS || `${window.location.protocol === 'https:' ? 'wss' : 'ws'}://${window.location.host}/ws`,
   VERSION: import.meta.env.VITE_VERSION,
   GIT_URL: "https://github.com/TimP4w/Phi"
 };
